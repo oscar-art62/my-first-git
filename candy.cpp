d02cc5cd1b9f15b9a13ae86c15bb06ae2620039e
oscar <u114705021.mg14@nycu.edu.tw>
2025-11-04 15:57:57 +0800

    update

--- candy.cpp
+++ candy.cpp
@@ -8,17 +8,16 @@ int main(){
         int count=0,exchange = 0,ex=0,origin,a=0,b=1;
         copy=n;
         origin = n;
-        for(int i=2;i<(int)p.size();i++){
+        for(int i=2;i<static_cast<int>(p.size());i++){
             b*=10;
             a=a*10+(p[i]-'0');
         }
-        cout<<a<<" "<<b<<endl;
-        while((int)(copy*a/b)){
-            while((int)((n-1)*a/b)==(int)(copy*a/b)){
+        while(static_cast<int>(copy*a/b)){
+            while(static_cast<int>((n-1)*a/b)==static_cast<int>(copy*a/b)){
                 n--;
             }
-            count=count+(int)(n*a/b);
-            exchange=(int)(n*a/b);
+            count=count+static_cast<int>(n*a/b);
+            exchange=static_cast<int>(n*a/b);
             n=exchange+copy-n;
             copy=n;
         }
---
+++ keyword_search_and_marking.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstring>
+#include<cctype>
+#include<string>
+using namespace std;
+
+int main(){
+    int i,j;
+    char line[101],code[31];
+    cin.getline(line,101);
+    cin>>code;
+    for(i=strlen(line)-1;i>=0;i--){
+        for(j=0;j<strlen(line);j++){
+            if(i<j) break;
+            char sub[101];
+            strncpy(sub,line+j,i-j+1);
+            sub[i-j+1]='\0';
+            if(strcasecmp(sub,code)==0){
+                cout<<j<<endl;
+                for(int k=0;k<strlen(line);k++){
+                    if(k==j){
+                        cout<<"[";
+                    }
+                    cout<<line[k];
+                    if(k==i){
+                        cout<<"]";
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+    cout<<-1<<'\n'<<line;
+    
+    return 0;
+
+    
+}
